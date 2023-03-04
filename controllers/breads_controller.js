f14a319bf5bba145087e05c3b8c800a48e3faa52
Casey Halbohn <wrxbeauty15@hotmail.com>
2023-03-04 11:26:15 -0500

    Added seed route

--- controllers/breads_controller.js
+++ controllers/breads_controller.js
@@ -1,9 +1,9 @@
-// Dependencies
+// DEPENDENCIES
 const express = require('express')
 const breads = express.Router()
 const Bread = require('../models/bread.js')
 
-// Index
+// INDEX
 breads.get('/', (req, res) => {
     Bread.find()
         .then(foundBreads => {
@@ -11,69 +11,52 @@ breads.get('/', (req, res) => {
                 breads: foundBreads,
                 title: 'Index Page'
             })
-            // console.log(foundBreads)
         })
-    // //res.send('This is the index at /breads')
-    // res.render('index', {
-    //     "breads": Bread,
-    //     "title": "Breads Index Title"
-    // });
 })
 
-// New
+// NEW
 breads.get('/new', (req, res) => {
     res.render('new')
 })
 
-// Show
+// SHOW
 breads.get('/:id', (req, res) => {
     Bread.findById(req.params.id)
-    .then(foundBread => {
-        console.log(foundBread)
-        res.render('show', {
-            bread: foundBread
+        .then(foundBread => {
+            console.log(foundBread)
+            res.render('show', {
+                bread: foundBread
+            })
+        }).catch(err => {
+            res.send('404')
         })
-    }).catch(err => {
-        res.send('404')
-    })
-    // if (Bread[req.params.arrayIndex]) {
-    //     res.render('Show', {
-    //         bread: Bread[req.params.arrayIndex],
-    //         index: req.params.arrayIndex,
-    //     })
-    // } else {
-    //     res.send('404')
-    // }
 })
 
-// Edit
+// EDIT
 breads.get('/:id/edit', (req, res) => {
     Bread.findById(req.params.id)
         .then(foundBread => {
-            res.render('edit',{
+            res.render('edit', {
                 bread: foundBread
-        // bread: Bread[req.params.indexArray],
-        // index: req.params.indexArray
+            })
         })
-    })
 })
 
-
-// Update
+// UPDATE
 breads.put('/:id', (req, res) => {
     if (req.body.hasGluten === 'on') {
         req.body.hasGluten = true
     } else {
         req.body.hasGluten = false
     }
-    Bread.findByIdAndUpdate(req.params.id, req.body, {new: true})
+    Bread.findByIdAndUpdate(req.params.id, req.body, { new: true })
         .then(updateBread => {
             console.log(updateBread)
             res.redirect(`/breads/${req.params.id}`)
         })
 })
 
-// Create
+// CREATE
 breads.post('/', (req, res) => {
     if (!req.body.image) {
         req.body.image = undefined
@@ -84,16 +67,46 @@ breads.post('/', (req, res) => {
         req.body.hasGluten = 'false'
     }
     Bread.create(req.body)
-    // Bread.push(req.body)
     res.redirect('/breads')
 })
 
-// Delete
+// DELETE
 breads.delete('/:id', (req, res) => {
     Bread.findByIdAndDelete(req.params.id)
         .then(deletedBread => {
             console.log(deletedBread)
             res.status(303).redirect('/breads')
+        })
+})
+
+// SEED
+breads.get('/data/seed', (req, res) => {
+    Bread.insertMany([
+        [
+            {
+                name: 'Rye',
+                hasGluten: true,
+                image: 'https://images.unsplash.com/photo-1595535873420-a599195b3f4a?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80',
+            },
+            {
+                name: 'French',
+                hasGluten: true,
+                image: 'https://images.unsplash.com/photo-1534620808146-d33bb39128b2?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80',
+            },
+            {
+                name: 'Gluten Free',
+                hasGluten: false,
+                image: 'https://images.unsplash.com/photo-1546538490-0fe0a8eba4e6?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1050&q=80',
+            },
+            {
+                name: 'Pumpernickel',
+                hasGluten: true,
+                image: 'https://images.unsplash.com/photo-1586444248902-2f64eddc13df?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1050&q=80',
+            }
+        ]
+    ])
+        .then(createdBreads => {
+            res.redirect('/breads')
         })
 })
 
